9bf571cd394892667f70281cd2e48030eeebd8d5
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-10-28 16:58:12 +0100

    Show examples of passing a maze file to Maze

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from graph import Graph
 from maze_solver import MazeSolver
 
 
-maze = Maze()
-print(maze.hashtag_str, '\n\n')
+def solve_maze(maze_file_name):
 
-width = maze.width
-height = maze.height
+    maze = Maze(open(maze_file_name + '.txt'))
 
-graph = Graph(maze, width, height)
-adjacency_list = graph.adjacency_list
+    print(maze.hashtag_str, '\n\n')
 
-maze_solver = MazeSolver(maze, adjacency_list)
-solved_path = maze_solver.solve()
+    width = maze.width
+    height = maze.height
 
-maze.draw_path(solved_path)
+    graph = Graph(maze, width, height)
+    adjacency_list = graph.adjacency_list
+
+    maze_solver = MazeSolver(maze, adjacency_list)
+    solved_path = maze_solver.solve()
+
+    maze.draw_path(solved_path)
+
+
+solve_maze('first_maze')
+
+print('\n\n')
+
+solve_maze('second_maze')
