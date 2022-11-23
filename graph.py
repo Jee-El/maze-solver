6e3184e5ddb4cb14e2f5d60edb6d0f1cd950c7a3
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-11-23 22:46:26 +0100

    Rename method so its return type is clear

--- graph.py
+++ graph.py
@@ -7,7 +7,8 @@ class Graph:
         self.height = height
         self.maze = maze
         self.vertices = tuple(
-            product(range(0, height + 1), range(0, width + 1)))
+            product(range(0, height + 1), range(0, width + 1))
+            )
         self.adjacency_list = self.build_adjacency_list()
 
     def build_adjacency_list(self, directions=((0, 1), (1, 0), (-1, 0), (0, -1))):
@@ -18,13 +19,17 @@ class Graph:
                 row = direction[0] + vertex[0]
                 column = direction[1] + vertex[1]
                 coordinates = row, column
-                if not self.valid_coordinates(coordinates):
+                if not self.are_valid_coordinates(coordinates):
                     continue
                 adjacent_vertices.append(coordinates)
             adjacency_list[vertex] = adjacent_vertices
         return adjacency_list
 
-    def valid_coordinates(self, coordinates):
+    def are_valid_coordinates(self, coordinates):
         row = coordinates[0]
         column = coordinates[1]
-        return (0 <= row <= self.height) and (0 <= column <= self.width) and (self.maze.to_bool[row][column])
+        return (
+                (0 <= row <= self.height)
+                and (0 <= column <= self.width)
+                and (self.maze.to_bool[row][column])
+               )
