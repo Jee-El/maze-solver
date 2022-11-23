0a8313cf16dd08d63a74bcf03673a0f821aba052
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-11-23 22:41:59 +0100

    Make the UX more user-friendly

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from maze import Maze
 from graph import Graph
 from maze_solver import MazeSolver
+from time import sleep
 
+nums_to_maze_fns = {
+    '1': 'dfs_worst_case',
+    '2': 'first_maze',
+    '3': 'second_maze',
+    '4': 'third_maze'
+}
 
-def solve_maze(maze_file_name):
+mazes_starts_ends = {
+    'dfs_worst_case': ((6, 0), (5, 0)),
+    'first_maze': ((0, 0), (10, 20)),
+    'second_maze': ((0, 0), (20, 30)),
+    'third_maze': ((0, 0), (64, 102))
+}
 
-    maze = Maze(open(maze_file_name + '.txt'))
+def solve_maze(maze_fn):
+    opened_maze_file = open(maze_fn + '.txt')
+
+    maze = Maze(opened_maze_file, *mazes_starts_ends[maze_fn])
 
     print(maze.hashtag_str, '\n\n')
+    sleep(1)
 
     width = maze.width
     height = maze.height
@@ -16,13 +32,21 @@ def solve_maze(maze_file_name):
     adjacency_list = graph.adjacency_list
 
     maze_solver = MazeSolver(maze, adjacency_list)
-    solved_path = maze_solver.solve()
 
-    maze.draw_path(solved_path)
+    bfs_solved_path = maze_solver.solve_with_bfs() 
+    maze.draw_path(bfs_solved_path)
 
+    print('\n\n')
 
-solve_maze('first_maze')
+    dfs_solved_path = maze_solver.solve_with_dfs() 
+    maze.draw_path(dfs_solved_path)
 
-print('\n\n')
+for num in nums_to_maze_fns:
+    fn = nums_to_maze_fns[num]
+    print(f'{num} : {fn}')
 
-solve_maze('second_maze')
+maze_to_solve = input('> Number : '))
+while maze_to_solve not in nums_to_maze_fns:
+    maze_to_solve = input('> Number : ')
+    
+solve_maze(nums_to_maze_fns[maze_to_solve])
