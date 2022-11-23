c7f2b40a097fac061ae19e3962d80e0c1fe96dba
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-11-23 22:42:34 +0100

    feat: add DFS as an option to solve mazes

--- maze_solver.py
+++ maze_solver.py
@@ -1,40 +1,98 @@
+from os import system
+from time import sleep
+
 class MazeSolver:
     def __init__(self, maze, adjacency_list):
         self.maze = maze
         self.adjacency_list = adjacency_list
+        self.dfs_explored_states = 0
+        self.bfs_explored_states = 0
 
-    def valid_coordinates(self, coordinates):
+    def are_valid_coordinates(self, coordinates):
         row = coordinates[0]
         column = coordinates[1]
-        if not (0 <= row <= self.maze.height):
-            return False
-        if not (0 <= column <= self.maze.width):
-            return False
-        if not (self.maze.to_bool[row][column]):
+        if  (
+                not (0 <= row <= self.maze.height)
+                or not (0 <= column <= self.maze.width)
+                or not (self.maze.to_bool[row][column])
+            ):
+            print('>> Invalid Coordinates')
             return False
         return True
 
-    def solve(self, start=None, end=None):
-        start = start or self.maze.start
-        end = end or self.maze.end
-        if not self.valid_coordinates(start) or not self.valid_coordinates(end):
-            return print('Invalid Coordinates')
-        if start == end:
-            return end
-        return self.bfs(start, end)
+    def solve_with_bfs(self, start=None, end=None):
+        start = self.maze.start
+        end = self.maze.end
 
-    def bfs(self, start, end):
+        for coordinates in (start, end):
+            if not self.are_valid_coordinates(coordinates):
+                return
+
+        return self.__bfs(start, end)
+
+    def solve_with_dfs(self):
+        start = self.maze.start
+        end = self.maze.end
+
+        for coordinates in (start, end):
+            if not self.are_valid_coordinates(coordinates):
+                return
+
+        return self.__dfs(start, end, [(start, [start])], {start})
+
+    def __bfs(self, start, end):
         queue = [(start, [start])]
-        visited = {start}
+        visited_vertices = {start}
 
         while queue:
             vertex, path = queue.pop(0)
+            self.bfs_explored_states += 1
+
+            # Uncomment the lines below to see the live version
+            # self.__clear_screen()
+            # self.maze.draw_path(path)
+            # sleep(1)
+
             if vertex == end:
+                print(f'Explored states: {self.bfs_explored_states}')
+                self.bfs_explored_states = 0
                 return path
+
             for adjacent_vertex in self.adjacency_list[vertex]:
-                if adjacent_vertex in visited:
+                if adjacent_vertex in visited_vertices:
                     continue
-                visited.add(adjacent_vertex)
+                visited_vertices.add(adjacent_vertex)
                 new_path = path + [adjacent_vertex]
                 queue.append((adjacent_vertex, new_path))
-        return []
+        raise Exception('No Solution')
+
+    def __dfs(self, start, end, queue, visited_vertices, explored_states = 0):
+        if not queue:
+            raise Exception('No Solution')
+        
+        vertex, path = queue.pop(0)
+        self.dfs_explored_states += 1
+
+        # Uncomment the lines below to see the live version
+        # self.__clear_screen()
+        # self.maze.draw_path(path)
+        # sleep(1)
+
+        if vertex == end:
+            print(f'Explored states: {self.dfs_explored_states}')
+            self.dfs_explored_states = 0
+            return path
+
+        for adjacent_vertex in self.adjacency_list[vertex]:
+            if adjacent_vertex in visited_vertices:
+                continue
+            visited_vertices.add(adjacent_vertex)
+            new_path = path + [adjacent_vertex]
+            new_queue = queue + [(adjacent_vertex, new_path)]
+            solved_path = self.__dfs(start, end, new_queue, visited_vertices, explored_states)
+
+            if solved_path:
+                return solved_path
+
+    def __clear_screen(self):
+        system('echo "\e[1;1H\e[2J"')
