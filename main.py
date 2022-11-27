40aa843d12ee583cbe8373caf94bf465176acf40
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-11-27 18:15:54 +0100

    feat: automate getting start and end cooridnates

--- main.py
+++ main.py
@@ -12,18 +12,10 @@ nums_to_maze_fns = {
   '4': 'third_maze'
 }
 
-mazes_starts_ends = {
-  'dfs_worst_case': ((6, 0), (5, 0)),
-  'first_maze': ((0, 0), (10, 20)),
-  'second_maze': ((0, 0), (20, 30)),
-  'third_maze': ((0, 0), (64, 102))
-}
-
-
 def solve_maze(maze_fn):
   opened_maze_file = open(maze_fn + '.txt')
 
-  maze = Maze(opened_maze_file, *mazes_starts_ends[maze_fn])
+  maze = Maze(opened_maze_file)
 
   graph = Graph(maze)
   adjacency_list = graph.adjacency_list
--- maze.py
+++ maze.py
@@ -1,27 +1,35 @@
 class Maze:
-  def __init__(self, maze_file, start, end):
-    self.start = start
-    self.end = end
-
+  def __init__(self, maze_file):
     self.hashtag_str = maze_file.read()
 
-    maze_file.seek(0)
-
-    self.hashtag_arr = self.hashtag_arr(maze_file)
+    self.hashtag_arr = self.to_hashtag_arr(maze_file)
 
     maze_file.close()
 
-    self.to_bool = self.to_bool(self.hashtag_arr)
+    self.start, self.end = self.start_and_end(self.hashtag_arr)
+
+    self.bool_list = self.to_bool_list(self.hashtag_arr)
 
     self.height = len(self.hashtag_arr)
     self.width = len(self.hashtag_arr[0])
 
-  def to_bool(self, maze_rows):
+  def to_bool_list(self, maze_rows):
     return [
         [True if char in [' ', 'A', 'B'] else False for char in row] for row in maze_rows
     ]
 
-  def hashtag_arr(self, maze_file):
+  def to_hashtag_arr(self, maze_file):
+    maze_file.seek(0)
     return [
         list(row.replace('\n', '')) for row in maze_file.readlines()
     ]
+
+  def start_and_end(self, maze):
+    start, end = None, None
+    for i in range(len(maze)):
+      if not start and 'A' in maze[i]:
+        start = (i, maze[i].index('A'))
+      if not end and 'B' in maze[i]:
+        end = (i, maze[i].index('B'))
+      if start and end:
+        return (start, end)
