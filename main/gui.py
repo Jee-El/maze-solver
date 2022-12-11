86796e52e4aceb1f73620c2cd2bd7b25434aa8f1
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-11 21:51:53 +0100

    Feat: optimize drawing the paths
    Before, it redrew the whole maze each time it was asked to draw a path,
    it also redrew all the manhattan distances.
    Now, it updates only the squares that make the path, it also
    updates only the manhattan distances of the squares that were updated.

--- main/gui.py
+++ main/gui.py
@@ -1,85 +1,107 @@
 import pygame
 
+
 class GUI:
-  def __init__(self, maze, maze_solver, algorithm_name):
-    pygame.display.set_caption('Maze Solver by Jee El')
+    def __init__(self, maze, maze_solver, algorithm_name):
+        pygame.display.set_caption("Maze Solver by Jee El")
 
-    self.maze = maze
-    self.maze_solver = maze_solver
-    self.is_gbfs = algorithm_name == 'gbfs'
-    self.solve = self.__get_algorithm(algorithm_name)
-    self.square_size = 50
-    self._screen = pygame.display.set_mode(
-      [self.maze.width * self.square_size, self.maze.height * self.square_size]
-    )
-    self.char_to_color = {
-      '#': 'black',
-      ' ': 'white',
-      'X': 'dodgerblue1',
-      'O': 'chartreuse3',
-      'A': 'red',
-      'B': 'green'
-    }
-    self._running = True
+        self.maze = maze
+        self.maze_solver = maze_solver
+        self.is_gbfs = algorithm_name == "gbfs"
+        self.__set_algorithm(algorithm_name)
 
-  def launch(self):
-    pygame.init()
-    self._font = pygame.font.SysFont('Arial', 25)
-    clock = pygame.time.Clock()
-    solved = False
+        self.square_size = 50
+        self.set_squares_margins()
+        self._screen = pygame.display.set_mode(
+            [self.maze.width * self.square_size, self.maze.height * self.square_size]
+        )
+        self.char_to_color = {
+            "#": "black",
+            " ": "white",
+            "X": "dodgerblue1",
+            "O": "chartreuse3",
+            "A": "red",
+            "B": "green",
+        }
+        self._running = True
 
-    while self._running:
-      clock.tick(60)
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          self._running = False
-
-      if not solved:
+    def launch(self):
+        pygame.init()
+        self._font = pygame.font.SysFont("Arial", 25)
+        clock = pygame.time.Clock()
         self.draw_maze()
         pygame.display.flip()
-        solved_path = self.solve(self)
-        solved = True
-        self.draw_maze(solved_path, True)
-        pygame.display.flip()
-    pygame.quit()
 
-  def draw_maze(self, path=[], solved=False):
-    self.__update_maze_list(path, solved)
-    top_margin = 0
-    for i in range(len(self.maze.list)):
-      left_margin = 0
-      for j in range(len(self.maze.list[i])):
-        char = self.maze.list[i][j]
-        self.draw_square(left_margin, top_margin, self.char_to_color[char])
-        if self.is_gbfs:
-          manhattan_distance = self.get_manhattan_distance((i, j), self.maze.end)
-          self.draw_manhattan_distance(left_margin, top_margin, manhattan_distance)
-        left_margin += self.square_size
-      top_margin += self.square_size
+        solved = False
+        while self._running:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
 
-  def draw_square(self, left_margin, top_margin, color):
-    square = pygame.Rect(
-      left_margin, top_margin, self.square_size, self.square_size
-    )
-    pygame.draw.rect(self._screen, color, square)
+            if not solved:
+                solved_path = self.solve(self)
+                solved = True
+                self.draw_path(solved_path, True)
+                pygame.display.flip()
+        pygame.quit()
 
-  def draw_manhattan_distance(self, left_margin, top_margin, manhattan_distance):
-    text = self._font.render(str(manhattan_distance), True, 'black')
-    left_margin += (self.square_size - text.get_width()) / 2
-    top_margin += (self.square_size - text.get_height()) / 2
-    self._screen.blit(text, (left_margin, top_margin))
-  
-  def get_manhattan_distance(self, start, end):
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+    def set_squares_margins(self):
+        self.squares_margins = {}
+        top_margin = 0
+        for i in range(self.maze.height):
+            left_margin = 0
+            for j in range(self.maze.width):
+                self.squares_margins[(i, j)] = (left_margin, top_margin)
+                left_margin += self.square_size
+            top_margin += self.square_size
 
-  def __update_maze_list(self, path, solved):
-    for row, col in path:
-      self.maze.list[row][col] = 'O' if solved else 'X'
+    def draw_maze(self):
+        for i in range(self.maze.height):
+            for j in range(self.maze.width):
+                char = self.maze.list[i][j]
+                self.draw_square(
+                    *self.squares_margins[(i, j)], self.char_to_color[char]
+                )
+                if self.is_gbfs:
+                    manhattan_distance = self.get_manhattan_distance(
+                        (i, j), self.maze.end
+                    )
+                    self.draw_manhattan_distance(
+                        *self.squares_margins[(i, j)], manhattan_distance
+                    )
 
-  def __get_algorithm(self, algorithm_name):
-    algorithms = {
-      'dfs': self.maze_solver.solve_with_dfs,
-      'bfs': self.maze_solver.solve_with_bfs,
-      'gbfs': self.maze_solver.solve_with_gbfs
-    }
-    return algorithms[algorithm_name]
+    def draw_path(self, path, solved=False):
+        for i, j in path:
+            self.draw_square(
+                *self.squares_margins[(i, j)],
+                self.char_to_color["O" if solved else "X"]
+            )
+            if self.is_gbfs:
+                manhattan_distance = self.get_manhattan_distance((i, j), self.maze.end)
+                self.draw_manhattan_distance(
+                    *self.squares_margins[(i, j)], manhattan_distance
+                )
+
+    def draw_square(self, left_margin, top_margin, color):
+        square = pygame.Rect(
+            left_margin, top_margin, self.square_size, self.square_size
+        )
+        pygame.draw.rect(self._screen, color, square)
+
+    def draw_manhattan_distance(self, left_margin, top_margin, manhattan_distance):
+        text = self._font.render(str(manhattan_distance), True, "black")
+        left_margin += (self.square_size - text.get_width()) / 2
+        top_margin += (self.square_size - text.get_height()) / 2
+        self._screen.blit(text, (left_margin, top_margin))
+
+    def get_manhattan_distance(self, start, end):
+        return abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+    def __set_algorithm(self, algorithm_name):
+        algorithms = {
+            "dfs": self.maze_solver.solve_with_dfs,
+            "bfs": self.maze_solver.solve_with_bfs,
+            "gbfs": self.maze_solver.solve_with_gbfs,
+        }
+        self.solve = algorithms[algorithm_name]
--- main/maze_solver.py
+++ main/maze_solver.py
@@ -1,113 +1,116 @@
 import pygame
 from queue import PriorityQueue
 
+
 class MazeSolver:
-  def __init__(self, maze, adjacency_list):
-    self.maze = maze
-    self.adjacency_list = adjacency_list
-    self.dfs_explored_states = 0
-    self.bfs_explored_states = 0
-    self.gbfs_explored_states = 0
+    def __init__(self, maze, adjacency_list):
+        self.maze = maze
+        self.adjacency_list = adjacency_list
+        self.dfs_explored_states = 0
+        self.bfs_explored_states = 0
+        self.gbfs_explored_states = 0
 
-  def solve_with_dfs(self, gui):
-    start = self.maze.start
-    end = self.maze.end
-    solution = self.__dfs(gui, start, end, [(start, [start])], {start})
-    return solution or self.__no_solution_found()
+    def solve_with_dfs(self, gui):
+        start = self.maze.start
+        end = self.maze.end
+        solution = self.__dfs(gui, start, end, [(start, [start])], {start})
+        return solution or self.__no_solution_found()
 
-  def solve_with_bfs(self, gui):
-    start = self.maze.start
-    end = self.maze.end
-    solution = self.__bfs(gui, start, end)
-    return solution or self.__no_solution_found()
+    def solve_with_bfs(self, gui):
+        start = self.maze.start
+        end = self.maze.end
+        solution = self.__bfs(gui, start, end)
+        return solution or self.__no_solution_found()
 
-  def solve_with_gbfs(self, gui):
-    start = self.maze.start
-    end = self.maze.end
-    solution = self.__gbfs(gui, start, end)
-    return solution or self.__no_solution_found()
-  
-  def __dfs(self, gui, start, end, queue, visited_vertices):      
-    vertex, path = queue.pop(0)
-    self.dfs_explored_states += 1
+    def solve_with_gbfs(self, gui):
+        start = self.maze.start
+        end = self.maze.end
+        solution = self.__gbfs(gui, start, end)
+        return solution or self.__no_solution_found()
 
-    while self.__enter_is_not_pressed():
-      pass
-    gui.draw_maze(path)
-    pygame.display.flip()
+    def __dfs(self, gui, start, end, queue, visited_vertices):
+        vertex, path = queue.pop(0)
+        self.dfs_explored_states += 1
 
-    if vertex == end:
-      print('Explored states : ', self.dfs_explored_states)
-      return path
+        while self.__enter_is_not_pressed():
+            pass
+        gui.draw_path(path)
+        pygame.display.flip()
 
-    for adjacent_vertex in self.adjacency_list[vertex]:
-      if adjacent_vertex not in visited_vertices:
-        visited_vertices.add(adjacent_vertex)
-        new_path = path + [adjacent_vertex]
-        new_queue = queue + [(adjacent_vertex, new_path)]
-        solved_path = self.__dfs(gui, start, end, new_queue, visited_vertices)
+        if vertex == end:
+            print("Explored states : ", self.dfs_explored_states)
+            return path
 
-        if solved_path:
-          return solved_path
+        for adjacent_vertex in self.adjacency_list[vertex]:
+            if adjacent_vertex not in visited_vertices:
+                visited_vertices.add(adjacent_vertex)
+                new_path = path + [adjacent_vertex]
+                new_queue = queue + [(adjacent_vertex, new_path)]
+                solved_path = self.__dfs(gui, start, end, new_queue, visited_vertices)
 
-  def __bfs(self, gui, start, end):
-    queue = [(start, [start])]
-    visited_vertices = {start}
+                if solved_path:
+                    return solved_path
 
-    while queue:
-      vertex, path = queue.pop(0)
-      self.bfs_explored_states += 1
+    def __bfs(self, gui, start, end):
+        queue = [(start, [start])]
+        visited_vertices = {start}
 
-      while self.__enter_is_not_pressed():
-        pass
-      gui.draw_maze(path)
-      pygame.display.flip()
+        while queue:
+            vertex, path = queue.pop(0)
+            self.bfs_explored_states += 1
 
-      if vertex == end:
-        print('Explored states : ', self.bfs_explored_states)
-        return path
+            while self.__enter_is_not_pressed():
+                pass
+            gui.draw_path(path)
+            pygame.display.flip()
 
-      for adjacent_vertex in self.adjacency_list[vertex]:
-        if adjacent_vertex not in visited_vertices:
-          visited_vertices.add(adjacent_vertex)
-          new_path = path + [adjacent_vertex]
-          queue.append((adjacent_vertex, new_path))
+            if vertex == end:
+                print("Explored states : ", self.bfs_explored_states)
+                return path
 
-  def __gbfs(self, gui, start, end):
-    priority_queue = PriorityQueue()
-    manhattan_distance = self.get_manhattan_distance(start, end) 
-    priority_queue.put((manhattan_distance, start, [start]))
-    visited_vertices = {start}
+            for adjacent_vertex in self.adjacency_list[vertex]:
+                if adjacent_vertex not in visited_vertices:
+                    visited_vertices.add(adjacent_vertex)
+                    new_path = path + [adjacent_vertex]
+                    queue.append((adjacent_vertex, new_path))
 
-    while not priority_queue.empty():
-      _, vertex, path = priority_queue.get()
-      self.gbfs_explored_states += 1
+    def __gbfs(self, gui, start, end):
+        priority_queue = PriorityQueue()
+        manhattan_distance = self.get_manhattan_distance(start, end)
+        priority_queue.put((manhattan_distance, start, [start]))
+        visited_vertices = {start}
 
-      while self.__enter_is_not_pressed():
-        pass
-      gui.draw_maze(path)
-      pygame.display.flip()
+        while not priority_queue.empty():
+            _, vertex, path = priority_queue.get()
+            self.gbfs_explored_states += 1
 
-      if vertex == end:
-        print('Explored states : ', self.gbfs_explored_states)
-        return path
+            while self.__enter_is_not_pressed():
+                pass
+            gui.draw_path(path)
+            pygame.display.flip()
 
-      for adjacent_vertex in self.adjacency_list[vertex]:
-        if adjacent_vertex not in visited_vertices:
-          visited_vertices.add(adjacent_vertex)
-          new_path = path + [adjacent_vertex]
-          manhattan_distance = self.get_manhattan_distance(adjacent_vertex, end)
-          priority_queue.put((manhattan_distance, adjacent_vertex, new_path))
-  
-  def get_manhattan_distance(self, start, end):
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+            if vertex == end:
+                print("Explored states : ", self.gbfs_explored_states)
+                return path
 
-  def __enter_is_not_pressed(self):
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RETURN:
-          return False
-    return True
+            for adjacent_vertex in self.adjacency_list[vertex]:
+                if adjacent_vertex not in visited_vertices:
+                    visited_vertices.add(adjacent_vertex)
+                    new_path = path + [adjacent_vertex]
+                    manhattan_distance = self.get_manhattan_distance(
+                        adjacent_vertex, end
+                    )
+                    priority_queue.put((manhattan_distance, adjacent_vertex, new_path))
 
-  def __no_solution_found(self):
-    raise Exception('No Solution was found')
+    def get_manhattan_distance(self, start, end):
+        return abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+    def __enter_is_not_pressed(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return False
+        return True
+
+    def __no_solution_found(self):
+        raise Exception("No Solution was found")
